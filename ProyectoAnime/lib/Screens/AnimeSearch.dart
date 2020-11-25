256992891c3b13e931743bd7619351a63cfa7bb0
Adolfo Espinosa <adolfo.espinosa@usm.cl>
2020-11-25 12:48:43 -0300

    crea lista scrowleable

--- ProyectoAnime/lib/Screens/AnimeSearch.dart
+++ ProyectoAnime/lib/Screens/AnimeSearch.dart
@@ -11,33 +11,48 @@ class _AnimeSearchState extends State<AnimeSearch> {
   @override
   Widget build(BuildContext context) {
     BuiltList<Search> _search = ModalRoute.of(context).settings.arguments;
-    String _imagen = _search.first.imageUrl;
-    String _tittle = _search.first.title;
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Image.asset(
-          "assets/iconologo.png",
-          height: 50,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Image.asset(
+            "assets/iconologo.png",
+            height: 50,
+          ),
         ),
-      ),
-      body: Column(
-        children: [
-          Card(
-            margin: EdgeInsets.symmetric(
-              vertical: 20.0,
-              horizontal: 25.0,
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Los Resultados de tu busqueda son'),
             ),
-            child: ListTile(
-              leading: Image.network(_imagen),
-              title: Text(
-                _tittle,
+            Container(
+              height: 625.0,
+              child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                itemCount: 15,
+                itemBuilder: (context, position) {
+                  return Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Card(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 1,
+                        horizontal: 15,
+                      ),
+                      child: ListTile(
+                        leading: Image.network(
+                          _search[position].imageUrl,
+                          height: 100,
+                          width: 50,
+                        ),
+                        title: Text(_search[position].title),
+                      ),
+                    ),
+                  );
+                },
               ),
             ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ));
   }
 }
