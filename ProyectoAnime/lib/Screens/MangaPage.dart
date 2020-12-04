e97de8dea8f4d70d62ce86fd2c88ce155fab9cbe
wardin123 <darwin.gutierrez@sansano.usm.cl>
2020-12-04 20:26:44 -0300

    traspaso completado

--- ProyectoAnime/lib/Screens/MangaPage.dart
+++ ProyectoAnime/lib/Screens/MangaPage.dart
@@ -1,10 +1,9 @@
 import 'package:ProyectoAnime/Widgets/Informaciongeneral.dart';
-import 'package:ProyectoAnime/Widgets/PartesuperiorPagAnime.dart';
 import 'package:ProyectoAnime/Widgets/Sinopsis.dart';
 import 'package:ProyectoAnime/widgets/ParteSuperiorManga.dart';
 import 'package:flutter/material.dart';
 
-class AnimePage extends StatelessWidget {
+class MangaPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
