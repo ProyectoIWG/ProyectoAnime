9233a8aa24b0a3cff814ecc33b537d3b37d3588c
wardin123 <darwin.gutierrez@sansano.usm.cl>
2020-12-04 20:21:40 -0300

    sinopsis implementada

---
+++ ProyectoAnime/lib/widgets/Sinopsis.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+// import 'package:ProyectoAnime/jikan_api.dart';
+
+class Sinopsis extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Card(
+          margin: EdgeInsets.symmetric(
+            vertical: 25.0,
+            horizontal: 100.0,
+          ),
+          child: ListTile(
+            title: Text(
+              'Sinopsis',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20.0,
+                fontFamily: 'Dosis',
+                color: Colors.pink,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+        Card(
+          margin: EdgeInsets.symmetric(
+            vertical: 5.0,
+            horizontal: 25.0,
+          ),
+          child: ListTile(
+            title: Text(
+              'Kaguya Shinomiya y Miyuki Shirogane son dos chicos prodigio que controlan el consejo de estudiantes de su prestigiosa academia, lo que las convierte en la crème de la crème de la élite. Ambos serían la pareja perfecta, si no fuera por su orgullo y narcisismo.',
+              style: TextStyle(
+                fontSize: 15.0,
+                fontFamily: 'Dosis',
+                color: Colors.pink,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
