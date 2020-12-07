96ebd8a1187c86b267288ad42f55ae5dfd3b25be
Adolfo Espinosa <adolfo.espinosa@usm.cl>
2020-12-07 15:24:46 -0300

    Update

--- ProyectoAnime/lib/Widgets/Informaciongeneral.dart
+++ ProyectoAnime/lib/Widgets/Informaciongeneral.dart
@@ -4,83 +4,87 @@ import 'package:link/link.dart';
 
 class Informaciongeneral extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Column( 
+  Widget build(BuildContext context) {
+    return Column(
       children: <Widget>[
-              Container(
-        margin: EdgeInsets.symmetric(
-                  vertical: 25.0,
+        Container(
+          margin: EdgeInsets.symmetric(
+            vertical: 25.0,
+          ),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                color: Colors.black,
+                width: 2.5,
+              )),
+          width: 200,
+          child: Column(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Text(
+                  "Informacion general: ",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ),
                 ),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(
-            color: Colors.black,
-            width: 2.5,
-          )
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(" "),
+              ),
+            ],
+          ),
         ),
-        width: 200,
-        child: Column( 
-          children: <Widget> [
-        Align(
-          alignment: Alignment.bottomCenter,
-         child:  Text("Informacion general: ", 
-         style: TextStyle(
-           fontSize: 20.0,
-         ),
-         ),
-        ),
-        Align(
-          alignment: Alignment.topLeft,
-         child:  Text(" "),
-        ),
-          ],
-        ),
-      ),
         Card(
-                child: Column( 
-          children: <Widget> [
-        Align(
-          alignment: Alignment.bottomLeft,
-         child:  Text("Titulo: ", 
-         style: TextStyle(
-           fontSize: 15.0,
-         ),
-         ),
-        ),
-        Align(
-          alignment: Alignment.bottomLeft,
-         child:  Text("Tipo: ", 
-         style: TextStyle(
-           fontSize: 15.0,
-         ),
-         ),
-        ),
-        Align(
-          alignment: Alignment.bottomLeft,
-         child:  Text("Status: ", 
-         style: TextStyle(
-           fontSize: 15.0,
-         ),
-         ),
-        ),
-        Align(
-          alignment: Alignment.bottomLeft,
-         child:  Text("Duración: ", 
-         style: TextStyle(
-           fontSize: 15.0,
-         ),
-         ),
-        ),
-       Align(
-          alignment: Alignment.bottomLeft,
-         child: Link(
-           child: Text("Link: Ver Kaguya-sama: Love is War"),
-           url: "https://www3.animeflv.net/ver/kaguyasama-wa-kokurasetai-tensaitachi-no-renai-zunousen-1"
-         ),
-        ),
-          ],
+          child: Column(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Text(
+                  "Titulo: ",
+                  style: TextStyle(
+                    fontSize: 15.0,
+                  ),
                 ),
+              ),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Text(
+                  "Tipo: ",
+                  style: TextStyle(
+                    fontSize: 15.0,
+                  ),
                 ),
+              ),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Text(
+                  "Status: ",
+                  style: TextStyle(
+                    fontSize: 15.0,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Text(
+                  "Duración: ",
+                  style: TextStyle(
+                    fontSize: 15.0,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Link(
+                    child: Text("Link: Ver Kaguya-sama: Love is War"),
+                    url:
+                        "https://www3.animeflv.net/ver/kaguyasama-wa-kokurasetai-tensaitachi-no-renai-zunousen-1"),
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
