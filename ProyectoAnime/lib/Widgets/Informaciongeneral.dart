6e100229f8a04e1a24011e0bc09bfda2eb8601f2
Adolfo Espinosa <adolfo.espinosa@usm.cl>
2020-12-07 15:28:17 -0300

    Update Informaciongeneral.dart

--- ProyectoAnime/lib/Widgets/Informaciongeneral.dart
+++ ProyectoAnime/lib/Widgets/Informaciongeneral.dart
@@ -2,39 +2,39 @@ import 'package:flutter/material.dart';
 
 class Informaciongeneral extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Column( 
+  Widget build(BuildContext context) {
+    return Column(
       children: <Widget>[
-              Container(
-        margin: EdgeInsets.symmetric(
-                  vertical: 25.0,
+        Container(
+          margin: EdgeInsets.symmetric(
+            vertical: 25.0,
+          ),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                color: Colors.black,
+                width: 2.5,
+              )),
+          height: 45,
+          width: 200,
+          child: Column(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Text(
+                  "Informacion general: ",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ),
                 ),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(
-            color: Colors.black,
-            width: 2.5,
-          )
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(" "),
+              ),
+            ],
+          ),
         ),
-        height: 45,
-        width: 200,
-        child: Column( 
-          children: <Widget> [
-        Align(
-          alignment: Alignment.bottomCenter,
-         child:  Text("Informacion general: ", 
-         style: TextStyle(
-           fontSize: 20.0,
-         ),
-         ),
-        ),
-        Align(
-          alignment: Alignment.topLeft,
-         child:  Text(" "),
-        ),
-          ],
-        ),
-      ),
       ],
     );
   }
