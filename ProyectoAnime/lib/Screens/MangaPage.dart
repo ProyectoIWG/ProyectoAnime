777d7da9684e83b2ec8eec9046ff20f33971e451
wardin123 <darwin.gutierrez@sansano.usm.cl>
2020-12-08 23:11:19 -0300

    personajes

--- ProyectoAnime/lib/Screens/MangaPage.dart
+++ ProyectoAnime/lib/Screens/MangaPage.dart
@@ -1,5 +1,6 @@
 import 'package:ProyectoAnime/Widgets/Informaciongeneral.dart';
 import 'package:ProyectoAnime/Widgets/Sinopsis.dart';
+import 'package:ProyectoAnime/Widgets/personajes.dart';
 import 'package:ProyectoAnime/widgets/ParteSuperiorManga.dart';
 import 'package:flutter/material.dart';
 
@@ -19,6 +20,7 @@ class MangaPage extends StatelessWidget {
           ParteSuperiorManga(),
           Sinopsis(),
           Informaciongeneral(),
+          Personajes(malId),
         ],
       ),
     );
