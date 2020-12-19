bc1e30f5ea5be4ddad580ac16c624d7bcb60e2d6
wardin123 <darwin.gutierrez@sansano.usm.cl>
2020-12-19 17:20:24 -0300

    fondo de pana

--- ProyectoAnime/lib/Screens/PagPersona.dart
+++ ProyectoAnime/lib/Screens/PagPersona.dart
@@ -6,21 +6,27 @@ class PagPersona extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     int malId = ModalRoute.of(context).settings.arguments;
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Image.asset(
-          "assets/iconologo.png",
-          height: 50,
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("assets/FondoPersonaje.jpeg"), fit: BoxFit.cover),
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Image.asset(
+            "assets/iconologo.png",
+            height: 50,
+          ),
         ),
+        body: ListView(
+          children: <Widget>[
+            PartesuperiorPersona(malId),
+            InfogeneralPagPersona(malId),
+          ],
+        ),
+        backgroundColor: Colors.black12,
       ),
-      body: ListView(
-        children: <Widget>[
-          PartesuperiorPersona(malId),
-          InfogeneralPagPersona(malId),
-        ],
-      ),
-      backgroundColor: Colors.black12,
     );
   }
 }
--- ProyectoAnime/lib/Screens/PersonajePage.dart
+++ ProyectoAnime/lib/Screens/PersonajePage.dart
@@ -6,21 +6,27 @@ class PersonajePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     int malId = ModalRoute.of(context).settings.arguments;
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Image.asset(
-          "assets/iconologo.png",
-          height: 50,
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("assets/FondoPersonaje.jpeg"), fit: BoxFit.cover),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Image.asset(
+            "assets/iconologo.png",
+            height: 50,
+          ),
+        ),
+        body: ListView(
+          children: <Widget>[
+            ParteSuperiorPersonaje(malId),
+            InfoGeneralPersonaje(malId),
+          ],
         ),
       ),
-      body: ListView(
-        children: <Widget>[
-          ParteSuperiorPersonaje(malId),
-          InfoGeneralPersonaje(malId),
-        ],
-      ),
-      backgroundColor: Colors.black,
     );
   }
 }
